bd35068e3bcfd1f564393676f98479ece402f9da
Dale Ramdeen <dramdeen1@gmail.com>
2019-12-17 21:13:27 -0500

    geolocation added

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,19 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 
-fetch('https://example.com')
+fetch('https://api.darksky.net/forecast/3d33ca9c7e52f847d713d8c3740d5f79/37.8267,-122.4233')
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    console.log(data.currently)
+    var info = JSON.stringify(data.currently);
+
+    router.get('/', function(req, res, next) {
+      res.render('index', { title: 'Weather', info: info });
+    });
+
   })
   .catch(err => err)
 
-  console.log('API Key:', process.env.API);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weather' });
-});
 
 module.exports = router;
