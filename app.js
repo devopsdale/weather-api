855ecabf63660ab27a780c6eb77343c3418aff51
Dale Ramdeen <dramdeen1@gmail.com>
2019-12-19 14:43:15 -0500

    have api working with cookies

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 
 
 var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -16,12 +19,12 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+//app.use(session({secret: 'weatherApiSecret', saveUninitialized: true, resave: true}));
 
 app.use('/', indexRouter);
-app.use('/geo', indexRouter);
-
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,12 +43,5 @@ app.use(function(err, req, res, next) {
 });
 
 
-// routes will go here
-app.get('/geo', function(req, res) {
-  var lat = req.param('lat');
-  var lon = req.param('lon');
-  console.log("---------------lat: "+lat+" lon: "+lon)
-  //res.send(user_id + ' ' + token + ' ' + geo);
-});
 
 module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,7 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 var cookieParser = require('cookie-parser');
-//var session = require('express-session');
-
-router.use(cookieParser());
-//router.use(session({secret: 'weatherApiSecret', saveUninitialized: true, resave: true}));
-
+var session = require('express-session');
 // router.use('/api', require('./api.v0/.js'));
 
 
@@ -32,45 +28,27 @@ router.use(cookieParser());
 // .catch(err => err)
 
 
-// router.get('/', function(req, res, next) {
-
-//   var info = req.query;
-//   var lat=34.0353778
-//   var lon=-84.2644238
+router.get('/currently', function(req, res, next) {
 
 
-
-//   console.log("lat: "+lat+" lon: "+lon);
-
-//   fetch('/api/currently')
-//   .then(response => response.json())
-//   .then(data => {
-//       var currently = JSON.stringify(data.currently);
-//       console.log(currently);
-//       res.render('index' );
-    
-//     })
-//   .catch(err => err)
-
-  
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  //console.log(req.query.lat);
-  res.cookie("lat", req.query.lat);
-  res.cookie("lon", req.query.lon);
-
-  fetch('/api/currently')
+  //console.log('lat: ', req.cookies.lat)
+  fetch('https://api.darksky.net/forecast/'+process.env.API+'/'+req.cookies.lat+','+req.cookies.lon)
   .then(response => response.json())
   .then(data => {
       var currently = JSON.stringify(data.currently);
+      //console.log('Location: '+lat)
+      res.send(currently);
+
+    
     })
   .catch(err => err)
 
+  //res.send("hi")
 
-  //console.log(req.session);
 
-  
-  res.render('index');
+
+
+
 });
 
 module.exports = router;
