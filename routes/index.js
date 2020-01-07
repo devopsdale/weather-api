61959110603cc0ef0abad95058201f855735998a
Dale Ramdeen <dramdeen1@gmail.com>
2020-01-07 11:42:08 -0500

    fixed frontend js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
@@ -8,16 +6,14 @@ var session = require('express-session');
 var moment = require('moment-timezone');
 
 router.get('/', function(req, res, next) {
-  console.log(req.cookies);
 
   if (!req.cookies.lat){
-    console.log("no luck");
-    console.log("no luck");
+    console.log("=============== no cookie found");
     res.render('index', {display: "flex", weatherdisplay: "none"} );
   }
   else
   {
-    console.log("we found a cookie!");
+    console.log(" ===============  we found a cookie!");
     fetch('https://api.openweathermap.org/data/2.5/weather?lat='+req.cookies.lat+'&lon='+req.cookies.lon+'&units=imperial&appid='+process.env.API)
     .then(response => response.json())
     .then(data => {
@@ -26,34 +22,13 @@ router.get('/', function(req, res, next) {
         var desc = weather[0].description;
         var icon = weather[0].icon; 
         var temp = Math.round(data.main.temp);
-
-        console.log(temp)
         var s = new Date(data.sys.sunrise).toLocaleDateString("en-US")
         var sys = data.sys;    
-        res.render('index', {display: "none", icon: icon, temp:temp, weatherdisplay: "flex", desc:desc , data: data, main:main, weather: weather[0]});
+        res.render('weather', {display: "none", icon: icon, temp:temp, weatherdisplay: "flex", desc:desc , data: data, main:main, weather: weather[0]});
       })
     .catch(err => err)
   }
 });
-
-router.post('/cookie', (req, res) => {
-  const zip = req.body.zip
-  console.log(zip)
-  fetch('https://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&units=imperial&appid='+process.env.API)
-  .then(response => response.json())
-  .then(data => {
-    var weather = data.weather;
-    var main = data.main;
-    var desc = weather[0].description;
-    var icon = weather[0].icon; 
-    console.log(data.sys.sunrise)
-    var s = new Date(data.sys.sunrise).toLocaleDateString("en-US")
-    var sys = data.sys;    
-    res.render('index', {display: "none", icon: icon, weatherdisplay: "flex", desc:desc , data: data, main:main, weather: weather[0]});
-    })
-  .catch(err => err)
-})
-
 
 module.exports = router;
                 
--- routes/weather.js
+++ routes/weather.js
@@ -11,6 +11,19 @@ router.get('/', function(req, res, next) {
   console.log("my long: " + req.query.lon);
   res.cookie("lat", req.query.lat);
   res.cookie("lon", req.query.lon);
-  res.end()
+  fetch('https://api.openweathermap.org/data/2.5/weather?lat='+req.query.lat+'&lon='+req.query.lon+'&units=imperial&appid='+process.env.API)
+  .then(response => response.json())
+  .then(data => {
+      var weather = data.weather;
+      var main = data.main;
+      var desc = weather[0].description;
+      var icon = weather[0].icon; 
+      var temp = Math.round(data.main.temp);
+      var s = new Date(data.sys.sunrise).toLocaleDateString("en-US")
+      var sys = data.sys;    
+      res.render('weather', {display: "none", icon: icon, temp:temp, weatherdisplay: "flex", desc:desc , data: data, main:main, weather: weather[0]});
+    })
+  .catch(err => err)
+
 });
 module.exports = router;
